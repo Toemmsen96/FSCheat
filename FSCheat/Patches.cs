56db8308a4ff1211055d18e434b6ecf298d7ab2f
Toemmsen96 <hotztom2@gmail.com>
2024-09-22 23:05:06 +0200

    Add project files.

---
+++ FSCheat/Patches.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bhvr.UnityShared;
+using HarmonyLib;
+using UnityEngine;
+
+namespace FSCheat
+{
+    internal class Patches
+    {
+
+        private static GameResources gRInstance;
+        
+        [HarmonyPatch(typeof(Application), "get_isEditor")]
+        [HarmonyPostfix]
+        private static void EnableEditor(ref bool __result)
+        {
+            __result = true;
+            Plugin.logger.LogInfo("Editor enabled");
+        }
+
+        /*
+        [HarmonyPatch(typeof(Storage), "HasResources")]
+        [HarmonyPostfix]
+        private static void InfiniteResources(ref bool __result, Storage __instance)
+        {
+            // Check if the instance or resources are null before applying the patch
+            if (__instance == null)
+            {
+                Plugin.logger.LogError("Storage or resources are null, cannot apply infinite resources.");
+                return;
+            }
+
+            // Apply your infinite resources logic if the check passes
+            __result = true;
+            Plugin.logger.LogInfo("Infinite resources enabled");  
+        }
+        */
+
+
+        [HarmonyPatch(typeof(GameResources), "get_Nuka")]
+        [HarmonyPostfix]
+        private static void InfiniteNuka(ref float __result, ref GameResources __instance)
+        {
+            //__result = 999999f;
+            //Plugin.logger.LogInfo("Infinite Nuka enabled");
+            gRInstance = __instance;
+            gRInstance.Nuka = 999999f;
+            //gRInstance.Food = 300f;
+            //gRInstance.Water = 999999f;
+            gRInstance.StimPack = 99f;
+            gRInstance.RadAway = 99f;
+            gRInstance.Lunchbox = 20f;
+            gRInstance.NukeColaQuantum = 999f;
+            gRInstance.MrHandy = 0f;
+            gRInstance.PetCarrier = 5f;
+            //gRInstance.Power = 400f;
+        }
+
+
+        /*
+        [HarmonyPatch(typeof(GameResources), "get_Food")]
+        [HarmonyPostfix]
+        private static void InfiniteFood(ref float __result, ref GameResources __instance)
+        {
+            __result = 999999f;
+            Plugin.logger.LogInfo("Infinite Food enabled");
+            gRInstance = __instance;
+        }
+        */
+
+    }
+}
---
+++ FSCheat/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+
+namespace FSCheat
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        private const string modGUID = "toemmsen.FSCheats";
+        private const string modName = "FSCheats";
+        private const string modVersion = "1.0.0";
+        private readonly Harmony harmony = new Harmony(modGUID);
+        internal static ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+        private static Plugin instance;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+
+            }
+
+            harmony.PatchAll(typeof(Patches));
+            harmony.PatchAll(typeof(Plugin));
+
+            logger.LogWarning((object)"\r\n" +
+                "  ______                                                                                                       \r\n" +
+                " /_  __/  ____   ___    ____ ___    ____ ___    _____  ___    ____    _____                                    \r\n" +
+                "  / /    / __ \\ / _ \\  / __ `__ \\  / __ `__ \\  / ___/ / _ \\  / __ \\  / ___/                                    \r\n" +
+                " / /    / /_/ //  __/ / / / / / / / / / / / / (__  ) /  __/ / / / / (__  )                                     \r\n" +
+                "/_/_____\\____/_\\___/ /_/ /_/_/_/ /_/ /_/ /_/_/____/  \\___/ /_/ /_/ /____/                            __        \r\n");
+            logger.LogInfo(modGUID+" loaded");
+        }
+    }
+}
