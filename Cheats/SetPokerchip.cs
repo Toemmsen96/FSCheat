ea4870edecefe5e729284f7dd90dcf458dba005e
Toemmsen96 <147745097+Toemmsen96@users.noreply.github.com>
2026-05-27 00:17:19 +0200

    renamed pokerchips to spin resources

--- Cheats/SetPokerchip.cs
+++ Cheats/SetPokerchip.cs
@@ -4,10 +4,10 @@ namespace FSCheat.Cheats
 {
     internal class SetPokerchip : CustomCommand
     {
-        public override string Name => "Set Pokerchips";
+        public override string Name => "Set Spin Resources (Pokerchip etc.)";
 
-        public override string Description => "Set Pokerchips to specified amount";
-        public override string Format => "/pokerchip <amount>";
+        public override string Description => "Set Spin resources (depends on seasons) to specified amount";
+        public override string Format => "/spinres <amount>";
         public override string Category => "Resources";
 
         public override void Execute(CommandInput message)
@@ -15,7 +15,7 @@ namespace FSCheat.Cheats
             GameResources resources = MonoSingleton<Vault>.Instance.Storage.Resources;
             if (!(message.Args[0].Length > 0))
             {
-                Utils.DisplayError("Message: Please specify an amount of Pokerchips to set.");
+                Utils.DisplayError("Message: Please specify an amount of Spin resources to set.");
                 return;
             }
             float amount = float.Parse(message.Args[0]);
@@ -25,7 +25,7 @@ namespace FSCheat.Cheats
                 return;
             }
             resources.PokerChip = amount;
-            Utils.DisplayMessage("Pokerchip Cheat: Set to " + message.Args[0]);
+            Utils.DisplayMessage("Spin Resources Cheat: Set to " + message.Args[0]);
         }
     }
 }
